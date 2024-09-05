d41be64b2ca89411acd84554ff1fb7c5c82bc812
Gustavo <melleugustavo@gmail.com>
2024-09-05 16:40:56 -0300

    : ultimo

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,8 +1,8 @@
-    function insert(num){
+function insert(num){
     var numero= document.getElementById('js-resultado').innerHTML ;
     document.getElementById('js-resultado').innerHTML = numero + num;
-    }
 
+}
 function clean(){
 
     document.getElementById('js-resultado').innerHTML = "";
@@ -13,8 +13,9 @@ function calcular(){
     var resultado = document.getElementById('js-resultado').innerHTML ;
     if(resultado){
         document.getElementById('js-resultado').innerHTML = eval(resultado);
-        // testeeee
     }
+
+   
 }
 
 function back(){
@@ -22,10 +23,14 @@ function back(){
     document.getElementById('js-resultado').innerHTML = resultado.substring(0,resultado.length -1);
 
 }
-function inverte(sinal){
-    var resultado = document.getElementById('js-resultado').innerHTML ;
-    var sinal= resultado.innerHTML * -1;
-    resultado.innerHTML = sinal;
 
+function inverter(){
+    var resultado = document.getElementById('js-resultado').innerHTML;
+    var sinal = document.getElementById('js-resultado').innerHTML = resultado * -1;
+
+    document.getElementById('js-resultado').innerHTML = sinal;
 
 }
+
+
+
