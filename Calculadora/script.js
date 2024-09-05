e04f7a2862059e0eee567915d4dae8aa6ab5e9f2
Gustavo <melleugustavo@gmail.com>
2024-09-05 14:38:31 -0300

    primeiro commit

---
+++ Calculadora/script.js
@@ -0,0 +1,30 @@
+    function insert(num){
+    var numero= document.getElementById('js-resultado').innerHTML ;
+    document.getElementById('js-resultado').innerHTML = numero + num;
+    }
+
+function clean(){
+
+    document.getElementById('js-resultado').innerHTML = "";
+}
+
+function calcular(){
+
+    var resultado = document.getElementById('js-resultado').innerHTML ;
+    if(resultado){
+        document.getElementById('js-resultado').innerHTML = eval(resultado);
+    }
+}
+
+function back(){
+    var resultado = document.getElementById('js-resultado').innerHTML;
+    document.getElementById('js-resultado').innerHTML = resultado.substring(0,resultado.length -1);
+
+}
+function inverte(sinal){
+    var resultado = document.getElementById('js-resultado').innerHTML ;
+    var sinal= resultado.innerHTML * -1;
+    resultado.innerHTML = sinal;
+
+
+}
